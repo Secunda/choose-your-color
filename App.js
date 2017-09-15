c151a99700b1d2fde1c79409c0f1837741abcfc2
Mr_Secunda <alexander.snesar@kredito.de>
2017-09-15 11:43:06 +0300

    added project structure

--- App.js
+++ App.js
@@ -1,10 +1,3 @@
-import React from 'react';
-import {StyleSheet, Text, View, StatusBar} from 'react-native';
-import {StackNavigator} from 'react-navigation';
-import Home from './Home';
-import Home1 from './Home1';
+import Routes from './app/routes';
 
-export default StackNavigator({
-    Home: {screen: Home},
-    Home1: {screen: Home1},
-});
+export default Routes;
---
+++ app/routes.js
@@ -0,0 +1,12 @@
+import {StackNavigator} from 'react-navigation';
+import Home from './screens/Home';
+import Settings from './screens/Settings';
+import Result from './screens/Result';
+import Game from './screens/Game';
+
+export default StackNavigator({
+    home: {screen: Home},
+    settings: {screen: Settings},
+    result: {screen: Result},
+    game: {screen: Game},
+});
